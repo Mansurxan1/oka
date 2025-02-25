8fcaecf6c02c3e84c1c82155064a26f6cf21193f
Mansurxan <Mansurxan.uz>
2025-02-25 16:24:52 +0500

    fdds

--- src/App.jsx
+++ src/App.jsx
@@ -80,49 +80,58 @@
 
 // export default App;
 
-
-
-
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { setShops } from "./redux/shopSlice";
+// import { useEffect } from "react";
+// import { useDispatch } from "react-redux";
+// import { setShops } from "./redux/shopSlice";
 import AppRouter from "./Router/AppRouter";
-import BranchModal from "./components/BranchModal";
-import axios from "axios";
-import i18n from "./i18n/i18n";
+// import BranchModal from "./components/BranchModal";
+// import axios from "axios";
+// import i18n from "./i18n/i18n";
 
-function App() {
-  const dispatch = useDispatch();
+// function App() {
+//   const dispatch = useDispatch();
 
-  useEffect(() => {
-    const storedLang = localStorage.getItem("language");
-    if (storedLang) {
-      i18n.changeLanguage(storedLang);
-    }
+//   useEffect(() => {
+//     const storedLang = localStorage.getItem("language");
+//     if (storedLang) {
+//       i18n.changeLanguage(storedLang);
+//     }
 
-    const fetchBranches = async () => {
-      try {
-        const response = await axios.get(
-          `${import.meta.env.VITE_API_URL}/shop`
-        );
-        dispatch(setShops(response.data));
-      } catch (error) {
-        console.error("Xatolik:", error);
-      }
-    };
+//     const fetchBranches = async () => {
+//       try {
+//         const response = await axios.get(
+//           `${import.meta.env.VITE_API_URL}/shop`
+//         );
+//         dispatch(setShops(response.data));
+//       } catch (error) {
+//         console.error("Xatolik:", error);
+//       }
+//     };
 
-    fetchBranches();
-    const interval = setInterval(fetchBranches, 5000);
+//     fetchBranches();
+//     const interval = setInterval(fetchBranches, 5000);
 
-    return () => clearInterval(interval);
-  }, [dispatch]);
+//     return () => clearInterval(interval);
+//   }, [dispatch]);
 
+//   return (
+//     <>
+//       <AppRouter />
+//       <BranchModal />
+//     </>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+
+const App = () => {
   return (
-    <>
+    <div>
       <AppRouter />
-      <BranchModal />
-    </>
+    </div>
   );
-}
+};
 
 export default App;
